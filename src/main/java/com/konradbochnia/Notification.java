43d921264c8be055ab78561bbd01b3077fcc4e09
Konrad Bochnia <kbochnia@gmail.com>
2018-03-15 16:16:57 +0100

    Move NotificationBody to be the inner class of the Notification class

--- src/main/java/com/konradbochnia/Notification.java
+++ src/main/java/com/konradbochnia/Notification.java
@@ -1,5 +1,7 @@
 package com.konradbochnia;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 public class Notification {
     private final NotificationBody notification;
     private final String to;
@@ -7,6 +9,31 @@ public class Notification {
     public Notification(String title, String body, String icon, String to) {
         this.notification = new NotificationBody(title, body, icon);
         this.to = "/topics/" + to;
+    }
+    
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class NotificationBody {
+        private final String title;
+        private final String body;
+        private final String icon;
+
+        public NotificationBody(String title, String body, String icon) {
+            this.title = title;
+            this.body = body;
+            this.icon = icon;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getBody() {
+            return body;
+        }
+
+        public String getIcon() {
+            return icon;
+        }
     }
 
     public NotificationBody getNotification() {
